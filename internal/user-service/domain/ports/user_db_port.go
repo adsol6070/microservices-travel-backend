00f35360df76e68e19ec3ebb5616b6e2eac131ad
jatin-90920 <goyaljatin208@gmail.com>
2025-01-31 17:18:19 +0530

    Some Changes

--- internal/user-service/domain/ports/user_db_port.go
+++ internal/user-service/domain/ports/user_db_port.go
@@ -6,7 +6,6 @@ type UserRepositoryPort interface {
 	Create(user models.User) (*models.User, error)
 	GetByID(id string) (*models.User, error)
 	GetByEmail(email string) (*models.User, error)
-	ResetPassword(token string, newPassword string) error
 	GetAll() ([]models.User, error)
 	Update(id string, user models.User) (*models.User, error)
 	Delete(id string) error
